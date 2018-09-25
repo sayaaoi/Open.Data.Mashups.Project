e00587bac40a6112ef8911cfb680e0b7a3f4003e
Amber Wu <yuejun.wu1115@gmail.com>
2018-09-25 08:16:54 -0500

    complete web scraping for one page

--- movie_names_lgbt.py
+++ movie_names_lgbt.py
@@ -12,4 +12,24 @@ soup = BeautifulSoup(origin_page.text, "html.parser")
 movie_name = ''
 for element in soup.find_all('a'):
     if element.get('title') is not None:
-        movie_name += (str(element.string) + '\n')
+        movie_name += (str(element.string) + "***")
+
+# Get movie names part only
+chunks = movie_name.split('edit***')
+for chunk in chunks:
+    if chunk.startswith('Z'):
+        z_index = chunks.index(chunk)
+    if chunk.startswith('$'):
+        a_index = chunks.index(chunk)
+
+movie_list = chunks[a_index : z_index+1]
+
+# convert each movie into an element of a list
+movie_names = []
+for movie_chunk in movie_list:
+    movie_temp = movie_chunk.split('***')
+    movie = movie_temp[:-1]
+    movie_names.extend(movie)
+    
+# a list of all lgbt movie names from Wiki page
+print(movie_names)
