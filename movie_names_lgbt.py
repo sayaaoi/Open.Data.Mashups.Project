a3ff6fc5b376fe000e29d88719c9dee2242db1fb
Amber Wu <yuejun.wu1115@gmail.com>
2018-09-25 00:23:42 -0500

    scrape and parse --> raw file

--- movie_names_lgbt.py
+++ movie_names_lgbt.py
@@ -8,8 +8,8 @@ origin_page = req.get("https://en.wikipedia.org/wiki/List_of_LGBT-related_films"
 
 soup = BeautifulSoup(origin_page.text, "html.parser")
 # print(soup.prettify())
-# print(soup.find_all('a'))
 
-for link in soup.find_all('a'):
-    # print(link.get('title'))
-    print(link.string)
+movie_name = ''
+for element in soup.find_all('a'):
+    if element.get('title') is not None:
+        movie_name += (str(element.string) + '\n')
