21fb6bb7e125c7f33599125932ba6dfea9104622
Amber Wu <yuejun.wu1115@gmail.com>
2018-11-04 12:13:12 -0600

    Revert "process election data in htm."
    This reverts commit 157b45fd91283648d6db947123f1685aaa3f0a33.

--- twitter_credentials.py
+++
@@ -1,6 +0,0 @@
-# Variables that contain the user credentials to access Twitter API.
-
-ACCESS_TOKEN = "1692129691-biA1cwnmNUsiSnN3Fr1MSVAjhld831l87fdvSTq"
-ACCESS_TOKEN_SECRET = "DfCgC5TV0buMzbgTdYAWmGFGG4kSnypmT9fhcCXblx4vS"
-CONSUMER_KEY = "Iiv9CuEL7A6sFc4rX0O7BXT62"
-CONSUMER_SECRET = "th2uEdpeaa1vLaOsY49rzIfs0n0oUTh71CHTqHKdtZsrxDieqF"
